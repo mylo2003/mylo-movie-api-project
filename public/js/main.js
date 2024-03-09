0a8c432b8c820fc563f3f22a3f65dcd2267ff97e
Camilo Mora <camiloalf23@gmail.com>
2024-03-09 18:35:49 -0500

    Avanzamos en diseño para mobiel, tablet y desktop, agregando swiper

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,69 @@
 import { API_KEY } from "/src/js/api-key.js";
 
 console.log(API_KEY);
+
+const swiper = new Swiper(".swiperHome", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const swiperCategories = new Swiper(".swiperCategories", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 8,
+      spaceBetween: 30,
+    },
+  },
+});
+
+const swiperMain = new Swiper(".swiperMain", {
+  effect: "cards",
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
